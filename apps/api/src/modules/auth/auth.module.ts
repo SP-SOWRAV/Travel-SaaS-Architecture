bbddac5e0b2ee86f823735f1a09459bd972c17cd
SP SOWRAV <sowravvatt@gmail.com>
2026-07-18 23:45:51 +0600

    Complete T12

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtAuthGuard } from '../../core/auth/jwt-auth.guard';
+import { RolesGuard } from '../../core/auth/roles.guard';
 import { CoreModule } from '../../core/core.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,6 +8,7 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [CoreModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard, RolesGuard],
+  exports: [JwtAuthGuard, RolesGuard],
 })
 export class AuthModule {}
